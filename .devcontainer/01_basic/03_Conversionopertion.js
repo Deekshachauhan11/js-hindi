70ab7477a4edc8d8b8b9259e539e98432f403a18
Deeksha chauhan <167565215+Deekshachauhan11@users.noreply.github.com>
2024-08-03 13:19:24 +0000

    Conversion

---
+++ .devcontainer/01_basic/03_Conversionopertion.js
@@ -0,0 +1,23 @@
+let score = undefined
+console.log(typeof score);
+console.log(typeof( score));
+
+let Name = "Deeksha"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn = "anu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "anu" => true
+
+
+let someNumber = "33"
+let stringNumber = String(someNumber)
+
+console.log( stringNumber);
