30c79242a558e4bd58ffb8b017235eebda96e36e
Deeksha chauhan <167565215+Deekshachauhan11@users.noreply.github.com>
2024-08-03 13:55:05 +0000

    update

--- .devcontainer/01_basic/03_Conversionopertion.js
+++ .devcontainer/01_basic/03_Conversionopertion.js
@@ -21,3 +21,38 @@ let someNumber = "33"
 let stringNumber = String(someNumber)
 
 console.log( stringNumber);
+
+// ************************ operations ********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " anu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
