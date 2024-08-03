5e513f5184dce8fa944bc70799e0eba4de834996
Deeksha chauhan <167565215+Deekshachauhan11@users.noreply.github.com>
2024-08-03 12:05:33 +0000

    Variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 122344
+let accountEmail = "cdeeksha0011@gmail.com"
+var accountNumber = "223344"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "anu@gmail.com"
+accountNumber = "123456"
+accountCity = "ghaziabad"
+
+console.table([accountId, accountEmail, accountNumber, accountCity, accountState]);
+
+// prefer not use var becasue of issue in block scope and funtional scope
