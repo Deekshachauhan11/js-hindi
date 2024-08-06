a37bf4efba6372390051ca0aa9ba71e54b33dda4
Deeksha chauhan <167565215+Deekshachauhan11@users.noreply.github.com>
2024-08-06 16:35:28 +0000

    update

--- .devcontainer/03_basic/02_scope.js
+++ .devcontainer/03_basic/02_scope.js
@@ -9,3 +9,47 @@ if (true) {
 console.log(a);
 // console.log(b);
 //console.log(c);
+
+
+//nested scope
+function one(){
+    const username = "anu"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+
+if (true){
+    const username = "deeksha"
+   if (username  === "deeksha"){
+        const website = " youtube"
+        //console.log(username + website);
+}
+   //console.log(website);
+   
+}
+//console.log(username);
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(6))
+function addone (num){
+    return num + 1
+}
+
+addtwo(6)
+const addtwo = function (num){
+    return num  + 2
+   
+}
+
+
+
